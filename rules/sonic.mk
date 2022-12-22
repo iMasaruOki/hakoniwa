18d40170452fba4588b12a8db24e0247b9b67170
Masaru OKI <masaru.oki@gmail.com>
2022-12-22 11:39:21 +0900

    Install authorized_keys intto sonic-vs.img.

--- rules/sonic.mk
+++ rules/sonic.mk
@@ -5,3 +5,6 @@ get-sonic-image: ${IMAGEDIR}/sonic-vs.img
 ${IMAGEDIR}/sonic-vs.img:
 	mkdir -p images
 	curl $(shell curl -s https://sonic.software/builds.json | jq .${SONIC_BRANCH}.\"sonic-vs.img.gz\".url) | gzip -d > $@ || rm $@
+	IMAGEDIR=$$(sudo virt-ls -a $@ -m /dev/sda3 / | grep image); \
+	sudo guestfish -a $@ -m /dev/sda3 mkdir-p /$$IMAGEDIR/rw/home/admin/.ssh; \
+	sudo guestfish -a $@ -m /dev/sda3 copy-in ./authorized_keys /$$IMAGEDIR/rw/home/admin/.ssh/
