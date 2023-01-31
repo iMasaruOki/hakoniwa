2dfa34551efaf077b025207b6ff31aa2f9860b80
Masaru OKI <masaru.oki@gmail.com>
2023-01-31 18:46:06 +0900

    Correct permission of /home/admin.

--- rules/sonic.mk
+++ rules/sonic.mk
@@ -7,4 +7,6 @@ ${IMAGEDIR}/sonic-vs.img:
 	curl $(shell curl -s https://sonic.software/builds.json | jq .${SONIC_BRANCH}.\"sonic-vs.img.gz\".url) | gzip -d > $@ || rm $@
 	IMAGEDIR=$$(sudo virt-ls -a $@ -m /dev/sda3 / | grep image); \
 	sudo guestfish -a $@ -m /dev/sda3 mkdir-p /$$IMAGEDIR/rw/home/admin/.ssh; \
-	sudo guestfish -a $@ -m /dev/sda3 copy-in ./authorized_keys /$$IMAGEDIR/rw/home/admin/.ssh/
+	sudo guestfish -a $@ -m /dev/sda3 chown 1000 1000 /$$IMAGEDIR/rw/home/admin/.ssh; \
+	sudo guestfish -a $@ -m /dev/sda3 copy-in ./authorized_keys /$$IMAGEDIR/rw/home/admin/.ssh/; \
+	sudo guestfish -a $@ -m /dev/sda3 chown 1000 1000  /$$IMAGEDIR/rw/home/admin/.ssh/authorized_keys
